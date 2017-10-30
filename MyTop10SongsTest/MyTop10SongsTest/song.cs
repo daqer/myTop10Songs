3996ba39e319cd7b8a43285c1ca21cd353de0cd0
daqer <david_christoffersen@live.dk>
2017-10-30 11:30:41 +0100

    first update
    test

---
+++ MyTop10SongsTest/MyTop10SongsTest/song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTop10SongsTest
+{
+    class Song
+    {
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+
+        public double Lenght { get; set; }
+
+
+    }
+}
